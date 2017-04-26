05430864c0c30160669ca30dec2ee3656be73105
DinkelLittlStar <c_dinkel@mail.fhsu.edu>
2017-04-26 12:28:42 -0500

    changed unc.py from stdin to fileinput

--- unc.py
+++ unc.py
@@ -1,13 +1,13 @@
 #! /usr/bin/python
 
-import sys
+import fileinput
 
 i = 0
 mean = 0
 meansum = 0
 sumdif = 0
 array = []
-for line in sys.stdin:
+for line in fileinput.input():
   line = line.strip()
   num = float(line)
   array.append(num)
